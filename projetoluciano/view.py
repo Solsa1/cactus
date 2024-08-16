18380c32c0a64bd1b15610e24f62b6b327b88c32
Solsa1 <151638811+Solsa1@users.noreply.github.com>
2024-08-16 11:57:14 -0300

    Update view.py

--- projetoluciano/view.py
+++ projetoluciano/view.py
@@ -4,11 +4,30 @@ from datetime import datetime
 #CONEXÃO COM O BANCO DE DADOS
 con = lite.connect('cactusbd.bd')
 
-#DEFININDO A FUNÇÃO DE INSERIR 
+#DEFININDO A FUNÇÃO DE INSERIR DO USUARIO
 
-def inserir_dados(i):
+def inserir_dados_user(i):
     cursor = con.cursor()
-    josefina = 'INSERT INTO usuario(id, nome, email, senha, biblioteca, operacoes, carrinho, foto) VALUES (?,?,?,?,?,?,?,?)' 
-    cursor.execute(josefina,i)
+    comando = 'INSERT INTO usuario(id, nome, email, senha, biblioteca, operacoes, carrinho, foto) VALUES (?,?,?,?,?,?,?,?)' 
+    cursor.execute(comando,i)
 
-def 
+#DEFININDO A FUNÇÃO DE DELETAR DO USUARIO
+
+def deletar_dados_user(i):
+    cursor = con.cursor()
+    comando = 'DELETE  FROM usuario WHERE id =?'
+    cursor.execute(comando, i)
+
+#DEFININDO A FUNÇÃO DE ATUALIZAR DO USUARIO
+
+def atualizar_dados_user(i):
+     cursor = con.cursor()
+     comando = 'UPDATE usuario SET nome=?, email=?, senha=?, foto=? WHERE id=?'
+     cursor.execute(comando,i)
+
+#DEFININDO A FUNÇÃO DE VER OS DADOS DO USUÁRIO
+
+def ver_dados_user(i):
+    cursor = con.cursor()
+    comando = 'SELECT * FROM usuario WHERE id=?'
+    cursor.execute(comando,i)
